bb6cd32946a730b7c664c2094226c55cada59e45
userAryush <khatriaryush@gmail.com>
2025-06-13 22:06:16 +0545

    django pagination system done

--- libraryms/settings.py
+++ libraryms/settings.py
@@ -87,7 +87,9 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend','rest_framework.filters.SearchFilter']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend','rest_framework.filters.SearchFilter'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
 
 
